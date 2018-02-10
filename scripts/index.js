cbb71d4fec86a34de83ef96589d2e4fbe8794214
Sydney Flak <sflak@porter-kresge-apts-57-101.resnet.ucsc.edu>
2018-02-10 10:16:50 -0800

    Hook up buttons

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#all").click(function() {
+        console.log("Show all clicked");
+    });
+    $("#todo").click(function() {
+        console.log("Show todo clicked");
+    });
+    $("#complete").click(function() {
+        console.log("Show complete clicked");
+    });
+});
